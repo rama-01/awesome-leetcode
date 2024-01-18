e0b81bdd4aceb3e9d658f2686afd91feb20f2c99
rama <ramastarzhu01@gmail.com>
2024-01-18 21:04:51 +0800

    finished the difference of two object

--- src/javascript专项练习/2700.ts
+++ src/javascript专项练习/2700.ts
@@ -1,15 +1,45 @@
 /* 限定o1,o2是对象或者数组 */
 
-const objDiff = (o1: any, o2: any) => {
+/* const objDiff = (o1: any, o2: any) => {
   const o: Record<string, any> = {}
-  if (typeof o1 !== typeof o2 || (Array.isArray(o1) ^ Array.isArray(o2))) return {}
   const k2 = Object.keys(o2)
   for (const key in o1) {
     if (k2.includes(key) && o1[key] !== o2[key]) {
-      if (typeof o1[key] === 'object' || typeof o2[key] === 'object') {
-        objDiff(o1[key], o2[key])
+      if (typeof o1[key] === typeof o2[key]) {
+        if (typeof o1[key] === 'object') {
+          if (
+            (Array.isArray(o1[key]) && Array.isArray(o2[key])) ||
+            (o1[key] instanceof Object && o2[key] instanceof Object)
+          ) {
+            o[key] = objDiff(o1[key], o2[key])
+          } else {
+            o[key] = [o1[key], o2[key]]
+          }
+        } else {
+          o[key] = [o1[key], o2[key]]
+        }
+      } else {
+        o[key] = [o1[key], o2[key]]
       }
-      o[key] = [o1[key], o2[key]]
+    }
+  }
+  return o
+} */
+
+/* refactor */
+const objDiff = (o1: any, o2: any) => {
+  const o: Record<string, any> = {}
+  const k2 = Object.keys(o2)
+  for (const key in o1) {
+    if (k2.includes(key) && o1[key] !== o2[key]) {
+      if (
+        Array.isArray(o1[key] && Array.isArray(o2[key])) ||
+        (o1[key] instanceof Object && o2[key] instanceof Object)
+      ) {
+        o[key] = objDiff(o1[key], o2[key])
+      } else {
+        o[key] = [o1[key], o2[key]]
+      }
     }
   }
   return o
@@ -26,6 +56,7 @@ const obj2 = {
   z: [1, 2, 3, [1]]
 }
 
-console.log(obj1, obj2)
+const res = JSON.stringify(objDiff(obj1, obj2), null, 2)
+console.log(res)
 
 export {}
