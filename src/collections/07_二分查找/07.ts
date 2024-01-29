9b85ef774a2e989212c0b60b8fb9a451a81a35c4
rama <ramastarzhu01@gmail.com>
2024-01-29 11:45:54 +0800

    fix: 修复了二分查找优化bug

--- src/collections/07_二分查找/07.ts
+++ src/collections/07_二分查找/07.ts
@@ -2,7 +2,7 @@ const binarySearch = (arr: Array<number>, target: number) => {
     // 1.定义左右指针
     let left = 0
     let right = arr.length - 1
-    // 2.移动左/右指针
+    // 2.移动左/右指针,思考这里循环的中止条件为什么是<=
     while (left <= right) {
         const mid = Math.floor((left + right) / 2)
         if (arr[mid] === target) {
--- src/剑指offer(专项突击版)/006/006.ts
+++ src/剑指offer(专项突击版)/006/006.ts
@@ -11,28 +11,30 @@ const solution = (numbers: Array<number>, target: number) => {
 
 /* 优化1 二分查找 */
 const solution2 = (numbers: Array<number>, target: number) => {
-    // for (let l = 0; l < n - 1; ) {
-    //     /*  while (l < r) {
-    //         const m = Math.floor((l + r) / 2)
-    //         if (numbers[m] < target - numbers[l]) {
-    //             break
-    //         } else if (numbers[m] > target - numbers[l]) {
-    //             r = m - 1
-    //         } else {
-    //             return [l, m]
-    //         }
-    //     } */
-    //     for (let r = n - 1; r > l; ) {
-    //         const m = Math.ceil((l + r) / 2)
-    //         if (numbers[m] < target - numbers[l]) {
-    //             l = m + 1
-    //         } else if (numbers[m] > target - numbers[l]) {
-    //             r = m - 1
-    //         } else {
-    //             return [l, m]
-    //         }
-    //     }
-    // }
+    for (let l = 0, n = numbers.length, r = n - 1; l < n - 1; l++) {
+        let lf = l + 1
+        while (lf <= r) {
+            const m = Math.floor((lf + r) / 2)
+            if (numbers[m] === target - numbers[l]) {
+                return [l, m]
+            } else if (numbers[m] < target - numbers[l]) {
+                lf = m + 1
+            } else {
+                r = m - 1
+            }
+        }
+        //     for (let r = n - 1; r > l; ) {
+        //         const m = Math.ceil((l + r) / 2)
+        //         if (numbers[m] < target - numbers[l]) {
+        //             l = m + 1
+        //         } else if (numbers[m] > target - numbers[l]) {
+        //             r = m - 1
+        //         } else {
+        //             return [l, m]
+        //         }
+        //     }
+        // }
+    }
     return -1
 }
 
@@ -71,8 +73,8 @@ function twoSum2(numbers: number[], target: number): number[] {
     }
 }
 
-const numbers = [2, 3, 4],
-    target = 6
+const numbers = [1, 2, 4, 6, 10],
+    target = 8
 console.log(solution2(numbers, target))
 
 export {}
