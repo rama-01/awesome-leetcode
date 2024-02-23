8743f38d1a2326576079f53e735a43976be34859
rama <ramastarzhu01@gmail.com>
2024-02-23 18:33:19 +0800

    tree data structure

--- src/python/problem-using-python/04_sort_search/11_merge_sort.py
+++ src/python/problem-using-python/04_sort_search/11_merge_sort.py
@@ -1,34 +1,35 @@
 def mergeSort(alist):
-    if len(alist) > 1:
-        mid = len(alist) // 2
-        leftHalf = alist[:mid]
-        rightHalf = alist[mid:]
+    if len(alist) <= 1:
+        return
+    mid = len(alist) // 2
+    leftHalf = alist[:mid]
+    rightHalf = alist[mid:]
 
-        mergeSort(leftHalf)
-        mergeSort(rightHalf)
+    mergeSort(leftHalf)
+    mergeSort(rightHalf)
 
-        i = 0
-        j = 0
-        k = 0
+    i = 0
+    j = 0
+    k = 0
 
-        while i < len(leftHalf) and j < len(rightHalf):
-            if leftHalf[i] <= rightHalf[j]:
-                alist[k] = leftHalf[i]
-                i += 1
-            else:
-                alist[k] = rightHalf[j]
-                j += 1
-            k += 1
-        # 处理可能剩余的元素
-        while i < len(leftHalf):
+    while i < len(leftHalf) and j < len(rightHalf):
+        if leftHalf[i] <= rightHalf[j]:
             alist[k] = leftHalf[i]
             i += 1
-            k += 1
-        while j < len(rightHalf):
+        else:
             alist[k] = rightHalf[j]
             j += 1
-            k += 1
-        return alist
+        k += 1
+    # 处理可能剩余的元素
+    while i < len(leftHalf):
+        alist[k] = leftHalf[i]
+        i += 1
+        k += 1
+    while j < len(rightHalf):
+        alist[k] = rightHalf[j]
+        j += 1
+        k += 1
+    return alist
 
 
 print(mergeSort([4, 3, 1, 5, 2]))
---
+++ src/python/problem-using-python/04_sort_search/12_quick_sort.py
@@ -0,0 +1,27 @@
+def quickSort(alist):
+    def partition(left, right):
+        if left >= right:
+            return
+        # 确定基准值pivot
+        pivot = alist[right]
+        # 必须定义左右指针
+        i = left
+        j = right - 1
+        while i <= j:
+            while alist[i] < pivot:
+                i += 1
+            while alist[j] > pivot:
+                j -= 1
+            if i <= j:
+                alist[i], alist[j] = alist[j], alist[i]
+                i += 1
+                j -= 1
+        alist[i], alist[right] = alist[right], alist[i]
+        partition(left, j)
+        partition(i + 1, right)
+
+    partition(0, len(alist) - 1)
+    return alist
+
+
+print(quickSort([4, 2, 5, 3, 1]))
