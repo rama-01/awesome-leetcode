8216bef5aa49d0e32090a4e882357e14a2881cd5
rama <ramastarzhu01@gmail.com>
2024-02-26 18:45:09 +0800

    binary search tree

---
+++ src/python/problem-using-python/05_tree/02_BinarySearchTree.py
@@ -0,0 +1,101 @@
+﻿class TreeNode:
+    def __init__(self, key, val, left=None, right=None, parent=None):
+        self.key = key
+        self.payload = val
+        self.leftChild = left
+        self.rightChild = right
+        self.parent = parent
+
+    def hasLeftChild(self):
+        return self.leftChild
+
+    def hasRightChild(self):
+        return self.rightChild
+
+    def isLeftChild(self):
+        return self.parent and self.parent.leftChild == self
+
+    def isRightChild(self):
+        return self.parent and self.parent.rightChild == self
+
+    def isRoot(self):
+        return not self.parent
+
+    def isLeaf(self):
+        return not (self.rightChild or self.leftChild)
+
+    def hasAnyChildren(self):
+        return self.rightChild or self.leftChild
+
+    def hasBothChildren(self):
+        return self.rightChild and self.leftChild
+
+    def replaceNodeData(self, key, value, lc, rc):
+        self.key = key
+        self.payload = value
+        self.leftChild = lc
+        self.rightChild = rc
+        if self.hasLeftChild():
+            self.leftChild.parent = self
+        if self.hasRightChild():
+            self.rightChild.parent = self
+
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+        self.size = 0
+
+    # 私有插入树节点方法
+    @staticmethod
+    def _put(self, key, val, currentNode):
+        if key < currentNode.key:
+            if currentNode.hasLeftChild():
+                self._put(key, val, currentNode.leftChild)
+            else:
+                currentNode.leftChild = TreeNode(key, val, parent=currentNode)
+        else:
+            if currentNode.hasRightChild():
+                self._put(key, val, currentNode.rightChild)
+            else:
+                currentNode.rightChild = TreeNode(key, val, parent=currentNode)
+
+    def put(self, key, val):
+        if self.root:
+            self._put(key, val, self.root)
+        else:
+            self.root = TreeNode(key, val)
+        self.size += 1
+
+    # 检索给定的树节点
+    @staticmethod
+    def _get(self, key, currentNode):
+        if not currentNode:
+            return None
+        else:
+            if key == currentNode.key:
+                return currentNode
+            elif key < currentNode.key:
+                return self._get(key, currentNode.leftChild)
+            else:
+                return self._get(key, currentNode.rightChild)
+
+    def get(self, key):
+        if not self.root:
+            return None
+        else:
+            res = self._get(key, self.root)
+            if res:
+                return res.payload
+            else:
+                return None
+
+    def __getitem__(self, key):
+        return self.get(key)
+
+    # 实现in操作
+    def __contains__(self, key):
+        if self._get(key, self.root):
+            return True
+        else:
+            return False
