6b3c951ea762236c332fb60165436d7b3edfe0a4
rama <ramastarzhu01@gmail.com>
2024-03-04 10:39:43 +0800

    perf: 优化深拷贝对象方法循环引用问题

--- src/collections/02_深拷贝/02_深拷贝.ts
+++ src/collections/02_深拷贝/02_深拷贝.ts
@@ -1,20 +1,46 @@
 const deepClone = <T>(obj: T) => {
-  // 判断obj是基本类型或null，直接返回
-  if (typeof obj !== 'object' || obj === null) {
-    return obj
-  }
-  // 判断obj是对象还是数组
-  const clone: any = Array.isArray(obj) ? [] : {}
-  for (const key in obj) {
-    if (Object.prototype.hasOwnProperty.call(obj, key)) {  //判断对象自身是否包含指定的属性,这样可以避免遍历到原型链上的属性。
-      clone[key] = deepClone(obj[key])
+    // 判断obj是基本类型或null，直接返回
+    if (typeof obj !== 'object' || obj === null) {
+        return obj
     }
-  }
-  return clone
+    // 判断obj是对象还是数组
+    const clone: any = Array.isArray(obj) ? [] : {}
+    for (const key in obj) {
+        if (Object.prototype.hasOwnProperty.call(obj, key)) {
+            //判断对象自身是否包含指定的属性,这样可以避免遍历到原型链上的属性。
+            clone[key] = deepClone(obj[key])
+        }
+    }
+    return clone
+}
+
+// refactor: 处理循环引用的问题
+const deepClone2 = <T>(obj: T, visited = new Map()) => {
+    if (visited.has(obj)) {
+        return visited.get(obj)
+    }
+    if (typeof obj !== 'object' || obj === null) {
+        return obj
+    }
+    const copy: any = Array.isArray(obj) ? [] : {}
+    visited.set(obj, copy)
+    for (const key in obj) {
+        if (Object.prototype.hasOwnProperty.call(obj, key)) {
+            copy[key] = deepClone(obj[key])
+        }
+    }
+    return copy
 }
 
 const o = { a: { b: { c: 1 } } }
-const copy = deepClone(o)
-console.log(copy)
+// const copy = deepClone(o)
+// console.log(copy)
+
+const copy2 = deepClone2(o)
+console.log(copy2);
+copy2.a.b.c = 3
+console.log(copy2,o);
+
+
 
 export {}
